56071eb1c409ab4ec872626e8ee41999405070bd
BANO.notIT <bano.notit@yandex.ru>
2018-11-17 21:26:05 +0300

    Добавляем модель пользователя и работу с БД

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import logging
 
 from flask import Flask, request
 
+import db
+
 app = Flask(__name__)
+
+# Инициализируем все таблички в бд
+db.create_tables()
 
 # Логируем все что у нас есть в gunicorn, чтобы было видно в консоли
 if __name__ != '__main__':
@@ -17,6 +22,7 @@ def hello_world():
     return 'Hello World!'
 
 
+# Обрабатываем телеграмовы сообщения
 @app.route('/telegram-handler', methods=['POST'])
 def telegram():
     app.logger.info('Telegram updates: {}'.format(request.get_json().__repr__()))
---
+++ db.py
@@ -0,0 +1,36 @@
+from enum import Enum, unique
+from os import environ
+
+from peewee import Model, IntegerField, PostgresqlDatabase, TextField
+
+database = PostgresqlDatabase(environ['DATABASE'])
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+@unique
+class UserState(Enum):
+    authorizing = 1
+    base = 2
+    integrating_tg = 3
+    integrating_vk = 4
+    trading = 5
+
+    @classmethod
+    def as_choices(cls):
+        return [reversed(item) for item in cls.__members__.items()]
+
+
+class User(BaseModel):
+    tg_id = IntegerField(null=True, unique=True, help_text='Telegram User Id')
+    vk_id = IntegerField(null=True, unique=True, help_text='VK User Id')
+    state = IntegerField(default='creating', choices=UserState.as_choices())
+    additional_parameter = TextField()
+
+
+def create_tables():
+    with database:
+        database.create_tables([User])
