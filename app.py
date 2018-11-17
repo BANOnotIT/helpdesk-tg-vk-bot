212f13e9fadaf814e6b41aa73f14c7130ce647f1
BANO.notIT <bano.notit@yandex.ru>
2018-11-17 20:11:03 +0300

    Добавляем обработчик для Телеги

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from logging import debug
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/telegram-handler')
+def telegram(request):
+    debug('Telegram updates: {}'.format(request.get_json().__repr__()))
+    return 'Ok'
 
 
 if __name__ == '__main__':
