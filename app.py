07f78b94d8f5f3755850b8fcec0eaff90f572ca5
BANO.notIT <bano.notit@yandex.ru>
2018-11-17 20:34:13 +0300

    Перемещаем логирование в gunicorn

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from logging import debug
+import logging
 
 from flask import Flask, request
 
 app = Flask(__name__)
+
+# Логируем все что у нас есть в gunicorn, чтобы было видно в консоли
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    # Учитываем уровень логов самого gunicorn
+    app.logger.setLevel(gunicorn_logger.level)
 
 
 @app.route('/')
@@ -12,7 +19,7 @@ def hello_world():
 
 @app.route('/telegram-handler')
 def telegram():
-    debug('Telegram updates: {}'.format(request.get_json().__repr__()))
+    app.logger.info('Telegram updates: {}'.format(request.get_json().__repr__()))
     return 'Ok'
 
 
