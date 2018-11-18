ce3fe070b429d3bfade2ea20b5a7fd7e11eb8db0
BANO.notIT <bano.notit@yandex.ru>
2018-11-19 00:21:18 +0300

    Добавил комментарии к коду пользователя и сделал метод для статуса

--- bot_brains.py
+++ bot_brains.py
@@ -4,35 +4,40 @@ from api import NMessage, MessageType as MsgType, Platform
 from db import UserState, User
 from utils import get_random_phrase
 
+# Ссылки на нашего бота в разных системах
 tg_link = 'https://t.me/itsolschool_heizenberg_bot'
 vk_link = 'https://vk.com/im?sel=-174147611'
 
 
 def process_nmessage(message: NMessage):
+    # Переменные для удобства
     user = message.user
     state = UserState(user.state)
+    is_from_vk = message.platform is Platform.vk
 
+    # Пользователь пожелал отменить текущее действие
     cancel = message.kind is MsgType.command and message.text == '/cancel'
-    from_vk = message.platform is Platform.vk
 
+    # Пользователь пришёл в первый раз и вообще это его первое сообщение
     if state is UserState.initial:
-        user.state = UserState.authorizing.value
-        user.state_param = ''
+        # Переводим нашего пользователя в статус авторизации
+        user.set_state(UserState.authorizing)
         user.save()
 
+        # Приветствуем пользователя и сразу говорим, что он должен пройти обряд инициализации
         message.reply('I\'m the cook. I\'m the man who killed Gus Fring. Say my name.\n{}'.format(repr(user)))
 
     elif state is UserState.authorizing:
         # Проверяем, есть ли переход в другое состояние машины
         if 'heizenberg' in message.text.lower() or cancel:
-            # Меняем состояние машины
-            user.state = UserState.base.value
-            user.state_param = ''
+            # Меняем состояние "машины" для этого конкретного пользователя
+            user.set_state(UserState.base)
             user.save()
 
             # Отвечаем пользователю
             message.reply('You\'re goddamn right! Now let\'s work!')
 
+            # Логируем, что в нашем полку прибыло
             current_app.logger.info('Authorization succeeded: {}'.format(repr(user)))
 
         else:
@@ -41,23 +46,31 @@ def process_nmessage(message: NMessage):
 
     # Основные состояния бота
     elif state is UserState.base:
+        # Обрабатываем команды
         if message.kind is MsgType.command:
-            if message.text == '/bind':
-                link = tg_link if from_vk else vk_link
 
+            # Пользователь изъявил желание интегрироваться ещё с чем-то
+            if message.text == '/bind':
+                # Получаем ссылку на бота в разных системах
+                link = tg_link if is_from_vk else vk_link
+
+                # Выбираем случайные 4 слова откуда-либо, главное случайные
                 phrase = get_random_phrase().lower()
 
-                user.state = UserState.integrating_vk.value
-                user.state_param = phrase
+                # Меняем состояние машины и передаём дополнительный аргумент к состоянию
+                user.set_state(UserState.integrating_vk, phrase)
                 user.save()
 
+                # Передаём пользователю инструкцию по интегрированию нового сервиса
                 message.reply('''
                 Go to {}.
                 When I'll have no doubt you are a good person to work with say `/in {}`. 
                 Then I would be sure you have both channels to contact me, ok?
                 '''.format(link, phrase))
 
+            # Обработка привязки одного аккаунта с другим
             elif message.text.startswith('/in'):
+
                 # Пропускаем первые 4 символа, обозначающие команду ("/in "), и берём остальное сообщение
                 phrase = message.text[3:]
 
@@ -73,17 +86,17 @@ def process_nmessage(message: NMessage):
 
                 # Берём нашего пользователя
                 n_user = User.get_or_none(
-                    user_integrating_tg if from_vk else user_integrating_vk,
+                    user_integrating_tg if is_from_vk else user_integrating_vk,
                     state_param=phrase
                 )
 
-                # Скорее всего такого пользователя нет
+                # Скорее всего такого пользователя нет, ну или фраза неправильная
                 if n_user is None:
                     message.reply('I don\'n know what you\'re talking about.')
                     return
 
                 # Производим слияние двух аккаунтов в один (суммируем деньги, переводим друзей, etc.)
-                if from_vk:
+                if is_from_vk:
                     user.tg = n_user.tg
                 else:
                     user.vk = n_user.vk
@@ -93,3 +106,11 @@ def process_nmessage(message: NMessage):
 
                 # Ну и теперь уже сохраняем текущего пользователя
                 user.save()
+
+                # Обязательно говорим, что кто-то совершил интеграцию
+                current_app.logger.info(
+                    '{} integrated {}'.format(
+                        repr(user),
+                        'tg' if is_from_vk else 'vk'
+                    )
+                )
